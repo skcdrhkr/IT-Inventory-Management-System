731bbd62d00a0238043fdefe8bd353a058328e4a
Gouthami95 <gouthami.mogili@accoliteindia.com>
2016-08-01 21:11:08 +0530

    Add files via upload
    Source Folder

---
+++ src/main/java/com/au/itinventory/Dao/EmployeeDao.java
@@ -0,0 +1,16 @@
+package com.au.itinventory.Dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.au.itinventory.Model.Employee;
+
+@Repository
+public class EmployeeDao {
+
+	public Employee getEmpDetails(Employee emp) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+	
+}
